7081f5c07c31b8e5cf275f957d2a026ec7daa84f
Suyash Dhakal <suyash.dhakal@icloud.com>
2025-09-14 00:08:28 +0545

    feat(authors): implement authors GET & POST endpoints

---
+++ src/controllers/authors.controller.js
@@ -0,0 +1,47 @@
+import {db} from '../../db/db.js';
+
+export const getAuthors = (req, res) => {
+    try {
+        const { name, order } = req.query;
+
+        let sql = `
+            SELECT authors.*, COUNT(books.id) as book_count
+            FROM authors
+            LEFT JOIN books ON authors.id = books.author_id
+        `;
+        const params = [];
+
+        // Filter by name (partial match)
+        if (name) {
+            sql += ' WHERE authors.name LIKE ? ';
+            params.push(`%${name}%`);
+        }
+
+        sql += ' GROUP BY authors.id ORDER BY book_count ' + (order?.toUpperCase() === 'ASC' ? 'ASC' : 'DESC');
+
+
+        db.all(sql, params, (err, rows) => {
+            if (err) {
+                res.status(500).json({ error: err.message });
+            } else {
+                res.status(200).json({ authors: rows });
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+export const createAuthor = (req, res) => {
+    try {
+        db.run('INSERT INTO authors (name, bio) VALUES (?, ?)', [req.body.name, req.body.bio], (err)=>{
+            if(err){
+                res.status(500).json({error: err.message});
+            }else{
+                res.status(201).json({message: "Author created successfully"});
+            }
+        });
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"});
+    }
+}
--- src/routes/authors.route.js
+++ src/routes/authors.route.js
@@ -1,6 +1,9 @@
 import express from 'express';
-
+import { createAuthor, getAuthors } from '../controllers/authors.controller.js';
 
 const router=express.Router();
+
+router.get('/', getAuthors);
+router.post('/', createAuthor);
 
 export default router;
