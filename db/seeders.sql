0c1fc9b51f8693d54fbfaf7a6714a91a689ff339
Suyash Dhakal <suyash.dhakal@icloud.com>
2025-09-15 17:55:57 +0545

    chore(seeder): seed database with more sample data

--- db/seeders.sql
+++ db/seeders.sql
@@ -4,11 +4,24 @@ INSERT INTO authors (name, email) VALUES
 ('Laxmi Prasad Devkota', 'laxmi.devkota@example.com'),
 ('Diamond Shamsher Rana', 'diamond.rana@example.com'),
 ('Subin Bhattarai', 'subin.bhattarai@example.com'),
-('Bishweshwar Prasad Koirala', 'bishweshwar.koirala@example.com');
+('Bishweshwar Prasad Koirala', 'bishweshwar.koirala@example.com'),
+('Bhanubhakta Acharya', 'bhanubhakta@example.com'),
+('Dhruba Chandra Gautam', 'dhruba.gautam@example.com'),
+('Yug Pathak', 'yug.pathak@example.com'),
+('Narayan Wagle', 'narayan.wagle@example.com'),
+('Sarubhakta', 'sarubhakta@example.com'),
+('Krishna Dharabasi', 'krishna.dharabasi@example.com');
 
 INSERT INTO books (title, isbn, published_year, author_id) VALUES
 ('Muna Madan', '9789937012', 1936, 1),
 ('Shakuntala', '9789937056', 1945, 2),
 ('Seto Bagh', '9789937091', 1957, 3),
 ('Summer Love', '9789937011', 2012, 4),
-('Sumnima', '9789937031', 1971, 5);
+('Sumnima', '9789937031', 1971, 5),
+('Ramayana (Nepali Translation)', '9789937101', 1854, 6),
+('Alikhit', '9789937102', 1979, 7),
+('Urgen Ko Ghoda', '9789937103', 2002, 8),
+('Palpasa Café', '9789937104', 2005, 9),
+('Pagal Basti', '9789937105', 1991, 10),
+('Radha', '9789937205', 2005, 11),              
+('Jhola', '9789937206', 2010, 11);
